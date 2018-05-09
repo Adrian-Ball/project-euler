24a33346a0aa105dcde6c688ed810c0bca07dfa5
adrian-ball <adrian.k.ball85@gmail.com>
2018-05-09 23:39:31 +1000

    Problem 18 - identical to that of P.67

---
+++ problem_18/problem_18.py
@@ -0,0 +1,33 @@
+#Project Euler Question 67
+
+#By starting at the top of the triangle below and moving to adjacent numbers 
+#on the row below, the maximum total from top to bottom is 23.
+
+# 3
+# 7 4
+# 2 4 6
+# 8 5 9 3
+
+# That is, 3 + 7 + 4 + 9 = 23.
+# Find the maximum total from top to bottom in 'this triangle'
+# Have manually generated a txt file from the question for this
+# and stored it under 'data/project-euler/p018_triangle.txt'
+#Code is identical to that of problem 67 as it is the exact same problem.
+
+file_path = '../../../data/project-euler/p018_triangle.txt'
+with open(file_path) as file:
+    #Treat the first line as a special case, no predecessors
+    prev_row = list(map(int, str.split(file.readline())))
+    for line in file:
+            curr_row = list(map(int, str.split(line)))
+            curr_row[0] += prev_row[0]
+            curr_row[-1] += prev_row[-1]
+            #Determine the larger of the two parent elements
+            #and then add to current element. 
+            for element in range(1, len(curr_row)-1):
+                max_prev = max(prev_row[element-1],prev_row[element])
+                curr_row[element] += int(max_prev)
+            prev_row = curr_row
+            
+answer = max(prev_row)
+print(answer)
